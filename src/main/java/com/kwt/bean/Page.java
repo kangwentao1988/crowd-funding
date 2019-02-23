9ec1a043b105dc456bd7a2d0024dbbcf2f21f0fe
kang <17601295911@163.com>
2019-02-23 18:29:20 +0800

    update 01

---
+++ src/main/java/com/kwt/bean/Page.java
@@ -0,0 +1,37 @@
+package com.kwt.bean;
+
+import java.util.List;
+
+public class Page<T> {
+
+	private List<T> data;
+	private int pageno;
+	private int totalno;
+	private int totalsize;
+	
+	public List<T> getData() {
+		return data;
+	}
+	public void setData(List<T> data) {
+		this.data = data;
+	}
+	public int getPageno() {
+		return pageno;
+	}
+	public void setPageno(int pageno) {
+		this.pageno = pageno;
+	}
+	public int getTotalno() {
+		return totalno;
+	}
+	public void setTotalno(int totalno) {
+		this.totalno = totalno;
+	}
+	public int getTotalsize() {
+		return totalsize;
+	}
+	public void setTotalsize(int totalsize) {
+		this.totalsize = totalsize;
+	}
+	
+}
